ac193735ae28a4f9cbd6a7145e6a030b0d2ffff9
dong si <dongsi@orbbec.com>
2023-08-08 16:32:23 +0800

    新增切换城市，手动更新功能

--- app/src/main/java/com/qingweather/android/ui/place/PlaceFragment.kt
+++ app/src/main/java/com/qingweather/android/ui/place/PlaceFragment.kt
@@ -12,6 +12,7 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.qingweather.android.MainActivity
 import com.qingweather.android.QingWeatherApplication
 import com.qingweather.android.R
 import com.qingweather.android.ui.weather.WeatherActivity
@@ -30,7 +31,7 @@ class PlaceFragment:Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        if(viewModel.isPlaceSaved()){
+        if(activity is MainActivity && viewModel.isPlaceSaved()){
             val place = viewModel.getSavedPlace()
             val intent = Intent(context,WeatherActivity::class.java).apply {
                 putExtra("location_lng",place.location.lng)
